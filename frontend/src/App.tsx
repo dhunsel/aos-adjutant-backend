996234c0d106e4981a34b1e7315f4c689873167f
Dennis Van hunsel <dennis.vanhunsel@outlook.com>
2026-04-23 19:04:42 +0200

    chore: import frontend into monorepo

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-amber-700 text-3xl font-bold">Aos Adjutant</h1>
+      <Button>Test button</Button>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/main.tsx
@@ -0,0 +1,29 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter } from "react-router";
+import "./index.css";
+import App from "./App.tsx";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      staleTime: 30_000,
+    },
+  },
+});
+
+const rootEl = document.getElementById("root");
+
+if (rootEl) {
+  createRoot(rootEl).render(
+    <StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </QueryClientProvider>
+    </StrictMode>,
+  );
+}
